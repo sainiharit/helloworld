9b393cc8d37dbdf5e8e71e55da2c5737a37b405d
helloworld5653 <56033056+helloworld5653@users.noreply.github.com>
2021-10-01 22:09:34 +0530

    Update main.js
    still getting error in runing burger menu

--- js/main.js
+++ js/main.js
@@ -132,6 +132,36 @@
 
 	};
 	carousel();
+	
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+     gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
 
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
@@ -165,6 +195,27 @@
     hideDistantElements: false,
     scrollProperty: 'scroll'
   });
+	
+	
+	// Burger Menu
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+
+			event.preventDefault();
+
+			if ( $('#ftco-nav').is(':visible') ) {
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');	
+			}
+
+			
+			
+		});
+
+	};
+	burgerMenu();
 
 	// scroll
 	var scrollWindow = function() {
